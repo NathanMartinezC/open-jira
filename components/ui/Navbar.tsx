c3c31ef1309dd2256d5cc627ebcbdc32f5568183
Nathan Martinez <n.mtz.cas@gmail.com>
2023-09-02 13:01:25 -0600

    Added Navbar component

---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,18 @@
+import { AppBar, IconButton, Toolbar, Typography } from '@mui/material';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOpenOutlined';
+
+export const Navbar = () => {
+    return (
+        <AppBar position='sticky'>
+            <Toolbar>
+                <IconButton
+                    size='large'
+                    edge='start'
+                >
+                    <MenuOutlinedIcon />
+                </IconButton>
+                <Typography variant='h6'>OpenJira</Typography>
+            </Toolbar>
+        </AppBar>
+    )
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,13 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 
-import { lightTheme } from '@/themes';
+import { darkTheme, lightTheme } from '@/themes';
 
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={ lightTheme }>
+    <ThemeProvider theme={ darkTheme }>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
