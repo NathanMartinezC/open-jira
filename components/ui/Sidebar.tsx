f9475f585cb6a1b46ed56b1f66e4e1ccc1b488b3
Nathan Martinez <n.mtz.cas@gmail.com>
2023-09-04 23:17:39 -0600

    Added UI context management

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,15 +1,21 @@
+import { useContext } from "react";
 import { Box, Divider, Drawer, Typography, List, ListItem, ListItemIcon, ListItemText } from "@mui/material"
 import MailOutlineOutlinedIcon from '@mui/icons-material/MailOutlineOutlined';
 import InboxOutlinedIcon from '@mui/icons-material/InboxOutlined';
 
+import { UIContext } from "@/context/ui";
+
 const menuItems: string[] = ['Inbox','Starred','Send Email','Drafts']
 
 export const Sidebar = () => {
+
+    const { sideMenuOpen, closeSideMenu } = useContext(UIContext)
+
     return (
         <Drawer
             anchor="left"
-            open={ true }
-            onClose={() => console.log('cerrando')}
+            open={ sideMenuOpen }
+            onClose={ closeSideMenu }
         >
             <Box sx={{ width: 250 }}>
                 <Box sx={{ padding: '5px 10px' }}>
