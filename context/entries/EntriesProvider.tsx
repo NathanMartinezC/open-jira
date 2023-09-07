d368a39e29ffe036701f45dffa2ac86f65925b4e
Nathan Martinez <n.mtz.cas@gmail.com>
2023-09-06 19:01:21 -0600

    Added NewEntry logic

--- context/entries/EntriesProvider.tsx
+++ context/entries/EntriesProvider.tsx
@@ -34,9 +34,24 @@ export const EntriesProvider:FC<PropsWithChildren> = ({ children }) => {
 
     const [state, dispatch] = useReducer(entriesReducer, Entries_INITIAL_STATE);
 
+    const addNewEntry = ( description: string ) => {
+
+        const newEntry: Entry = {
+            _id: uuidv4(),
+            description,
+            createdAt: Date.now(),
+            status:'pending'
+        }
+
+        dispatch({ type: '[Entry] Add-Entry', payload: newEntry })
+
+    }
+
     return (
         <EntriesContext.Provider value={{
-            ...state
+            ...state,
+            //Methods
+            addNewEntry,
         }}>
             { children }
         </EntriesContext.Provider>
