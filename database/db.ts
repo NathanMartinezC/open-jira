e8199f434742ef6d9abcfdbe490049cde29bcca6
Nathan Martinez <n.mtz.cas@gmail.com>
2023-09-07 20:31:51 -0600

    Added db connection base config

---
+++ database/db.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const mongooseConnection = {
+    isConnected: 0
+}
+
+export const connect = async() => {
+    if (mongooseConnection.isConnected) {
+        console.log("We are connected");
+        return;
+    }
+
+    if (mongoose.connections.length > 0) {
+        mongooseConnection.isConnected = mongoose.connections[0].readyState;
+
+        if (mongooseConnection.isConnected === 1) {
+            console.log('Using previous connection');
+            return;
+        }
+
+        await mongoose.disconnect();
+    }
+
+    await mongoose.connect(process.env.MONGO_URL || '')
+    console.log("Connected...")
+
+}
+
+export const disconnect = async() => {
+    if (mongooseConnection.isConnected === 0) return;
+
+    await mongoose.disconnect();
+    console.log("Disconnected...")
+}
