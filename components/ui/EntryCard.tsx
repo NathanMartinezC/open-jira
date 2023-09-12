7c46ad6a5d342e395ea61443c3db805a09e64fa7
Nathan Martinez <n.mtz.cas@gmail.com>
2023-09-11 23:23:31 -0600

    Added get data for form

--- components/ui/EntryCard.tsx
+++ components/ui/EntryCard.tsx
@@ -1,15 +1,18 @@
 import { DragEvent, FC, useContext } from 'react';
+import { useRouter } from 'next/router';
 import { Card, CardActionArea, CardActions, CardContent, Typography } from '@mui/material';
 import { Entry } from '@/interfaces';
 import { UIContext } from '@/context/ui';
+
 interface Props {
     entry: Entry
 }
 
-
 export const EntryCard:FC<Props> = ({ entry }) => {
 
     const { startDragging, endDragging } = useContext(UIContext);
+    const router = useRouter();
+
 
     const onDragStart = ( event: DragEvent) => {
         event.dataTransfer.setData('text', entry._id);
@@ -20,8 +23,13 @@ export const EntryCard:FC<Props> = ({ entry }) => {
         endDragging();
     }
 
+    const onClick = () => {
+        router.push(`/entries/${ entry._id }`);
+    }
+
     return (
         <Card
+            onClick={ onClick }
             sx={{ marginBottom: 1 }}
             draggable
             onDragStart={ onDragStart }
--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -1,17 +1,23 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, FC } from "react";
+import { GetServerSideProps } from "next";
 import { capitalize, Button, Card, CardActions, CardContent, CardHeader, FormControl, FormControlLabel, FormLabel, Grid, Radio, RadioGroup, TextField, IconButton } from "@mui/material";
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 import { Layout } from "@/components/layouts"
-import { EntryStatus } from "@/interfaces";
+import { Entry, EntryStatus } from "@/interfaces";
+import { dbEntries } from "@/database";
 
 
 const validStatus: EntryStatus[] = ['pending', 'in-progress', 'finished']
 
-export const EntryPage = () => {
+interface Props {
+    entry: Entry
+}
 
-    const [inputValue, setInputValue] = useState('');
-    const [status, setStatus] = useState<EntryStatus>('pending');
+export const EntryPage:FC<Props> = ( { entry } ) => {
+
+    const [inputValue, setInputValue] = useState(entry.description);
+    const [status, setStatus] = useState<EntryStatus>(entry.status);
     const [touched, setTouched] = useState(false);
 
     const isNotValid = useMemo(() => inputValue.length <= 0 && touched, [inputValue, touched]);
@@ -29,7 +35,7 @@ export const EntryPage = () => {
     }
 
     return (
-        <Layout title="... ... ...">
+        <Layout title={ inputValue.substring(0,15) + '...' }>
             <Grid
                 container
                 justifyContent='center'
@@ -38,7 +44,7 @@ export const EntryPage = () => {
                 <Grid item xs={ 12 } sm={ 8 } md={ 6 }>
                     <Card>
                         <CardHeader 
-                            title={`Entry: ${ inputValue }`}
+                            title={`Entry:`}
                             subheader={ `Created at ...` }
                         />
                         <CardContent>
@@ -104,5 +110,27 @@ export const EntryPage = () => {
         </Layout>
     );
 };
+
+export const getServerSideProps: GetServerSideProps = async({ params }) => {
+    
+        const { id } = params as { id: string };
+
+        const entry = await dbEntries.getEntryById(id);
+
+        if (!entry) {
+            return {
+                redirect: {
+                    destination: '/',
+                    permanent: false
+                }
+            }
+        }
+    
+        return {
+            props: {
+                entry
+            }
+        }
+    }
 
 export default EntryPage;
